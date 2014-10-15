d450e7332d94513b13bb6f39c8c54c90abbcf753
Benjamin Fox <benjaminf@intergen.co.nz>
2014-10-15 14:48:10 +1300

    Re-configured for new domain

--- src/ResearchTracker/app/src/main/java/com/microsoft/researchtracker/Constants.java
+++ src/ResearchTracker/app/src/main/java/com/microsoft/researchtracker/Constants.java
@@ -2,7 +2,7 @@ package com.microsoft.researchtracker;
 
 public final class Constants {
 
-    public static final String SHAREPOINT_URL = "https://foxintergen.sharepoint.com";
+    public static final String SHAREPOINT_URL = "https://foxintergenusa.sharepoint.com";
     public static final String SHAREPOINT_SITE_PATH = "ContosoResearchTracker";
 
     public static final String EXCHANGE_RESOURCE_ID = "https://outlook.office365.com/";
@@ -12,10 +12,10 @@ public final class Constants {
     public static final String[] EXCHANGE_MAIL_FIELDS_TO_SELECT = { "Id", "Subject", "Sender", "ToRecipients", "CcRecipients", "DateTimeSent", "ToRecipients", "LastModifiedTime", "ParentFolderId" };
     public static final String[] EXCHANGE_CONTACT_FIELDS_TO_SELECT = { "Id", "DisplayName", "Surname", "EmailAddress1", "JobTitle", "Department", "CompanyName", "MiddleName", "GivenName", "NickName" };
 
-    public static final String AAD_DOMAIN = "foxintergen.onmicrosoft.com";
-    public static final String AAD_CLIENT_ID = "13b04d26-95fc-4fb4-a67e-c850e07822a8";
+    public static final String AAD_DOMAIN = "foxintergenusa.onmicrosoft.com";
+    public static final String AAD_CLIENT_ID = "3a164325-14ba-471b-beb8-7b40a9f1a72e";
     public static final String AAD_AUTHORITY = "https://login.windows.net/common"; //"https://login.windows.net/" + AAD_DOMAIN;
-    public static final String AAD_REDIRECT_URL = "http://android/complete";
+    public static final String AAD_REDIRECT_URL = "http://example.com/redirect";
 
     public static final String AAD_RESOURCE_ID = SHAREPOINT_URL; //EXCHANGE_RESOURCE_ID;
 
